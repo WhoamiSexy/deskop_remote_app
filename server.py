5b896543994ee3090462fb55152409c357db6416
WhoamiSexy <152791183+WhoamiSexy@users.noreply.github.com>
2024-01-14 22:47:06 +0700

    thêm hàm nhận server

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 import socket
+import os
 from os import getlogin
 from PIL import Image, ImageGrab #Import thư viện ImageGrab từ Pillow để chụp ảnh màn hình.
 import io
 from io import BytesIO
-import cv2
 import numpy as np
 from random import randint
 import pyautogui
@@ -17,7 +17,7 @@ from queue import Queue
 
 
 print("[SERVER]: STARTED")
-server_address = ('192.168.0.79', 1234)
+server_address = ('172.16.26.138', 12345)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                
 sock.bind(server_address) # Server  
 sock.listen(5)
@@ -28,35 +28,17 @@ class Dekstop(QMainWindow):
         super().__init__()
         self.initUI()
 
-    # def ChangeImage(self, conn):
-    #     try:
-    #         while True:
-    #             img = ImageGrab.grab()
-    #             img_bytes = io.BytesIO()
-    #             img.save(img_bytes, format='PNG', quality=50)
-    #             conn.send(img_bytes.getvalue())
-    #     except:
-    #         conn.close()
-    def ChangeImage(self, conn):
+    def ChangeImage(self, conn): #_________________________________________________________________________________________
         try:
-            old_img = None
             while True:
                 img = ImageGrab.grab()
-                img_np = np.array(img)
-
-                if old_img is not None:
-                    delta = cv2.absdiff(old_img, img_np)
-                    img_bytes = io.BytesIO()
-                    Image.fromarray(delta).save(img_bytes, format='PNG')
-                    conn.send(img_bytes.getvalue())
-                else: #tấm ảnh đầu tiên
-                    img_bytes = io.BytesIO()
-                    Image.fromarray(img_np).save(img_bytes, format='PNG')
-                    conn.send(img_bytes.getvalue())
-                old_img = img_np
+                img_bytes = io.BytesIO()
+                img.save(img_bytes, format='PNG')
+                conn.send(img_bytes.getvalue())
         except:
             conn.close()
-    def Queue_solving(self, queue_):
+
+    def Queue_solving(self, queue_, conn): #______________________________________________________________________________
         try:
             print("Queue Started")
             while True:
@@ -65,31 +47,17 @@ class Dekstop(QMainWindow):
                 for message in messages:
                     if message:
                         print("Processing data:", message)
-                        # if message.startswith("mouse"):
-                        key, mouse_case, x, y, action, button  = message.split(',')
-                        self.Mouse_solving(mouse_case, x, y, action, button)
+                        if message.startswith("mouse"):
+                            key, mouse_case, x, y, action, button  = message.split(',')
+                            self.Mouse_solving(mouse_case, x, y, action, button)
+                        elif message.startswith("keyboard"):
+                            key, charc, action = message.split(',')
+                            self.Character_solving(charc, action, conn)
         except Exception as e:
             print(e)
             print("Queue Error")
 
-
-    def Queue1_solving(self, queue1, conn):
-        try:
-            print("Queue Started")
-            while True:
-                data = queue1.get()
-                messages = data.split()  # Split messages based on a delimiter
-                for message in messages:
-                    if message:
-                        print("Processing data:", message)
-                        key, charc, action = data.split(',')
-                        self.Character_solving(charc, action, conn)
-        except Exception as e:
-            print(e)
-            print("Queue Error")
-
-
-    def Mouse_solving(self, mouse_case, x, y, action, button):
+    def Mouse_solving(self, mouse_case, x, y, action, button): #___________________________________________________________
         try:
             if mouse_case.startswith("on_move"):
                 pyautogui.moveTo(int(x), int(y))
@@ -104,7 +72,7 @@ class Dekstop(QMainWindow):
         except:
             print("Mouse Error")
 
-    def Character_solving(self, charc, action, conn):
+    def Character_solving(self, charc, action, conn): #______________________________________________________________________
         try:
             if action.startswith("on_press"):
                 if charc.startswith("Key"):
@@ -120,6 +88,13 @@ class Dekstop(QMainWindow):
         except:
             print("Keyboard Error")
 
+    def receive_file(self, data): #_______________________________________________________________________________________
+        filename = data['file_name']
+        file_content = data['file_content']
+        filepath = os.path.join("D:\\", filename)
+        with open(filepath, 'wb') as f:
+            f.write(file_content)
+
     def initUI(self):
         self.MainProgram = Thread(target = self.Main_Program, daemon = True)
         self.MainProgram.start()
@@ -127,7 +102,7 @@ class Dekstop(QMainWindow):
     def Main_Program(self):
         # Khởi tạo Queue để xử lí dữ liệu từ Client
         self.queue_ = Queue()
-        self.queue1 =Queue()
+
         while True:
             conn, addr = sock.accept()
             with conn:
@@ -137,21 +112,14 @@ class Dekstop(QMainWindow):
                 self.output_thread = Thread(target = lambda: self.ChangeImage(conn), daemon = True)
                 self.output_thread.start()  
                 # Luồng nhận data từ Queue
-                self.input_thread = Thread(target = lambda: self.Queue_solving(self.queue_), daemon = True)    
+                self.input_thread = Thread(target = lambda: self.Queue_solving(self.queue_, conn), daemon = True)    
                 self.input_thread.start()  
-
-                self.input_threadboard = Thread(target = lambda: self.Queue1_solving(self.queue1, conn), daemon = True)    
-                self.input_threadboard.start()  
-               
                 try:
                     while(True):
                         data_nhận = conn.recv(99999)
                         data = data_nhận.decode('utf-8')
-                        #print(data)
-                        if data.startswith("keyboard"):
-                            self.queue1.put(data)
-                        elif data.startswith("mouse"):
-                            self.queue_.put(data)
+                        # print(data)
+                        self.queue_.put(data)
                 except Exception as e:
                     print(e)
                     print(f"Connection with {addr} closed")              
@@ -161,3 +129,4 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Dekstop()
     sys.exit(app.exec())
+    
