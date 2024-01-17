8be5d06faea7a46e11a2db08e4fd80ccd9d215e9
WhoamiSexy <152791183+WhoamiSexy@users.noreply.github.com>
2024-01-17 10:56:00 +0700

    huhu

--- Client.py
+++ Client.py
@@ -176,7 +176,28 @@ class Dekstop(QMainWindow):
         data = {'type': 'file_request', 'file_name': file_name}
         serialized_data = pickle.dumps(data)
         self.client_socket.send(serialized_data)
-
+    Cách 1:
+     def File_to_server(self):
+        options = QFileDialog.Options()
+        # options |= QFileDialog.DontUseNativeDialog
+        filename, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "", options = options)
+        if filename:
+            with open(filename, 'rb') as f:
+                file_content = f.read()
+                file_name = os.path.basename(filename)
+                data = {'type':'file_re', 'file_name': file_name, 'file_content': file_content}
+                serialized_data = pickle.dumps(data)
+                self.client_socket.send(serialized_data)
+    cách 2
+    self.SendFile = QPushButton(self.window2) # Nút gửi file
+        self.SendFile.move(70, 5)
+        self.SendFile.resize(460, 45)
+        self.SendFile.setStyleSheet("font-size: 25px")
+        self.SendFile.setText("Gửi file")
+        self.SendFile.clicked.connect(self.File_to_ser)
+    def File_to_ser(self):
+        self.Thread = Thread(target = File_to_server(), daemoion= true)
+        self.ThreadThread.start()
     # Gửi file qua server_____________________________________________________________________________________________
     def File_to_server(self):
         options = QFileDialog.Options()
