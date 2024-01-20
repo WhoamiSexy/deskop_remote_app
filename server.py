d1cc5276664e75b4b3056df1dd387e8fda449c23
WhoamiSexy <152791183+WhoamiSexy@users.noreply.github.com>
2024-01-20 17:43:01 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import socket
 import os
-from os import getlogin
 from PIL import Image, ImageGrab
 import io
 from io import BytesIO
@@ -11,6 +10,7 @@ from pynput.keyboard import Controller as KeyboardController, Key
 from pynput.mouse import Controller as MouseController, Button
 import traceback
 
+import threading
 from threading import Thread
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QLabel, QPushButton, QAction, QMessageBox
@@ -21,7 +21,7 @@ import pickle
 
 print("[SERVER]: STARTED")
 
-server_address = ('localhost', 1234)
+server_address = ('192.168.0.103', 1234)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                
 sock.bind(server_address) # Server  
 sock.listen(5)
@@ -36,6 +36,7 @@ class Dekstop(QMainWindow):
 
     def ChangeImage(self, conn):
         try:
+            old_img = None
             while True:
                 img = ImageGrab.grab()
                 img_bytes = io.BytesIO()
@@ -48,17 +49,7 @@ class Dekstop(QMainWindow):
                 # Then send the image data
                 conn.send(img_data)
         except:
-            conn.close()        
-    
-
-    # Thao tác server gửi file cho client_________________________________________________________________________________________
-    def send_file_list(self, client_socket): # Gửi danh sách file trong ổ đĩa___________________________________________________________
-        files = os.listdir('This PC:\\')
-        data = {'type': 'file_list', 'data': files}
-        serialized_data = pickle.dumps(data)
-        client_socket.send(serialized_data)
-
-
+            conn.close()       
     def Mouse_solving(self, data):
         try:
             if data['event_type'] == 'on_move':
@@ -74,7 +65,6 @@ class Dekstop(QMainWindow):
         except Exception as e:
             print("Mouse Error: ", e)
     def Character_solving(self, data, conn):
-        
         try:
             if data['action'] == 'on_press':
                 keyboard.press(data['key_name'])
@@ -83,35 +73,67 @@ class Dekstop(QMainWindow):
 
         except Exception as e:
             print("Keyboard Error: ", traceback.format_exc())
+    def recvall(self, sock, n):
+    # Helper function to recv n bytes or return None if EOF is hit
+        data = b''
+        while len(data) < n:
+            packet = sock.recv(n - len(data))
+            if not packet:
+                return None
+            data += packet
+        return data
+    def Receive_file(self, data):
+        file_name = data['file_name']
+        save_path = data['save_path']
+        file_size = data['file_size']
+
+        with open(save_path, 'wb') as file:
+            remaining_size = file_size
+            while remaining_size > 0:
+                chunk = self.recvall(self.conn, min(1024, remaining_size))
+                if not chunk:
+                    break
+                file.write(chunk)
+                remaining_size -= len(chunk)
+
+        print(f"File '{file_name}' received and saved at: {save_path}")
+
+        
     def initUI(self):
         self.MainProgram = Thread(target = self.Main_Program, daemon = True)
         self.MainProgram.start()
     
     def Main_Program(self):
         while True:
-            conn, addr = sock.accept()
-            with conn:
+            self.conn, self.addr = sock.accept()
+            with self.conn:
                 print("----------Connected----------")
-                print(f"Connected by {addr}")
+                print(f"Connected by {self.addr}")
                 # Luồng gửi data ảnh
-                self.output_thread = Thread(target = lambda: self.ChangeImage(conn), daemon = True)
+                self.output_thread = Thread(target = lambda: self.ChangeImage(self.conn), daemon = True)
                 self.output_thread.start()  
                 try:
                     while(True):
-                        data_received = conn.recv(1024)
+                        data_received = self.conn.recv(9999)
                         data = pickle.loads(data_received)
-                        print(data)
-                        if data['type']=='keyboard':
-                            self.Character_solving(data, conn)
-                        if data['type']=='mouse':
+                        
+                        if data['type'] == 'keyboard':
+                            self.Character_solving(data, self.conn)
+                        if data['type'] == 'mouse':
                             self.Mouse_solving(data)
+                        if data['type'] == 'file_re':
+                            self.Receive_file(data)
+                        if data['type'] == 'file_check':
+                            length = self.conn.recv(4)  # Assume that the length of the pickled object is sent first
+                            length = struct.unpack('!I', length)[0]
+                            data_received = self.recvall(self.conn, length)
+                            data = pickle.loads(data_received)
                 except Exception as e:
                     print('mainError: ', e)
-                    print(f"Connection with {addr} closed")              
+                    print(f"Connection with {self.addr} closed")              
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Dekstop()
     sys.exit(app.exec())
-
 
